f590fd112c3e97ee87f3469a2f965f1d979f8de8
kyoung <pmmpmm85@gmail.com>
2024-08-09 02:51:49 +0900

    Feat: [MOT-8] 다이얼로그 팝업

--- src/dialogue.ts
+++ src/dialogue.ts
@@ -1,1 +1,40 @@
+type DialogueBtn = 'image' | 'video' | 'note' | 'task';
 
+const dialogueBtns = document.querySelector('.dialogue-btns');
+const dialogueInpField = document.querySelector('.dialogue-input-field');
+const dialoguePop = document.querySelector('.dialogue-pop');
+const dialogueCloseBtn = document.querySelector('.dialogue-pop .closeBtn');
+
+dialogueBtns?.addEventListener('click', (e) => {
+  const target = e.target as HTMLButtonElement;
+  const name = target?.name as DialogueBtn;
+
+  if (dialogueInpField?.lastChild) dialogueInpField?.removeChild(dialogueInpField.lastChild);
+
+  if (name === 'image' || name === 'video') {
+    dialogueInpField?.insertAdjacentHTML(
+      'beforeend',
+      `<div>
+        <label for="url" class="block pb-1 font-medium">URL</label>
+        <input type="text" id="url" class="block w-full px-4 py-2 border border-solid border-inherit"
+        />
+      </div>`
+    );
+  }
+  if (name === 'note' || name === 'task') {
+    dialogueInpField?.insertAdjacentHTML(
+      'beforeend',
+      `<div>
+        <label for="body" class="block pb-1 font-medium">BODY</label>
+        <textarea name="body" id="body" rows="5" cols="33"
+          class="block w-full px-4 py-2 border border-solid border-inherit resize-none"></textarea>
+      </div>`
+    );
+  }
+
+  dialoguePop?.classList.add('open');
+});
+
+dialogueCloseBtn?.addEventListener('click', () => {
+  dialoguePop?.classList.remove('open');
+});
