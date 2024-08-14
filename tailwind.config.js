6836f959def25735439912f3b32046612e75709c
kyoung <pmmpmm85@gmail.com>
2024-08-14 19:32:19 +0900

    set commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./**/*.{html,js,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ['point-100']: 'rgba(194, 245, 47,1)',
+        ['basic-100']: 'rgba(37, 22, 26, 1)',
+        ['basic-70']: 'rgba(37, 22, 26, 0.7)',
+        ['basic-40']: 'rgba(37, 22, 26, 0.4)',
+      },
+    },
   },
   plugins: [],
 };
