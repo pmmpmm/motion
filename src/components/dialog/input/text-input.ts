2e2ea2aec1f2df67dbbcca139ce1d25d4df881f2
kyoung <pmmpmm85@gmail.com>
2024-08-25 17:31:02 +0900

    Feat: [MOT-16] 다이얼로그 input 빈 값 체크

--- src/components/dialog/input/text-input.ts
+++ src/components/dialog/input/text-input.ts
@@ -18,6 +18,7 @@ export class TextInputDialog extends BaseComponent<HTMLElement> implements TextI
           <label for="text-body" class="block pb-1 font-medium">BODY</label>
           <textarea name="body" id="text-body" rows="5" cols="33"
           class="body-inp block w-full px-4 py-2 border border-solid border-inherit whitespace-pre resize-none"></textarea>
+          <p class="mt-2 text-sm text-basic-70"><span class="font-medium">Task</span> 작성 시 줄바꿈 혹은 , 를 사용하시면 체크박스 목록이 생성됩니다.</p>
         </div>
       </div>`);
   }
